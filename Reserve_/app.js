0761eec5fbfae0d2ef9d70580f0a3b4313241c2e
IOTAI ROBERTO <124071310+IOTAIROBERTO@users.noreply.github.com>
2024-09-02 23:41:58 -0700

    .

--- Reserve_/app.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2020 8th Wall, Inc.
-
-import {selfDestructComponent, dropObjectComponent} from './components'
-AFRAME.registerComponent('self-destruct', selfDestructComponent)
-AFRAME.registerComponent('drop-object', dropObjectComponent)
